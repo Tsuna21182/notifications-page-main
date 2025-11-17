067a2aa9b227db64877042a0a142cc281b36a07a
Tsuna21182 <tsunadelanochecer@gmail.com>
2025-11-17 14:38:12 -0500

    correcion rutas imagenes

--- src/data/data.ts
+++ src/data/data.ts
@@ -15,7 +15,7 @@ export type DataProps = {
 
 export const Data: DataProps[] = [
   {
-    image: "/src/assets/images/avatar-mark-webber.webp",
+    image: "/public/assets/images/avatar-mark-webber.webp",
     name: "Mark Webber",
     description: "reacted to your recent post ",
     highlight: "My first tournament today!",
@@ -24,7 +24,7 @@ export const Data: DataProps[] = [
     id: 1,
   },
   {
-    image: "/src/assets/images/avatar-angela-gray.webp",
+    image: "/public/assets/images/avatar-angela-gray.webp",
     name: "Angela Gray",
     description: "followed you",
     date: "5m ago",
@@ -32,7 +32,7 @@ export const Data: DataProps[] = [
     id: 2,
   },
   {
-    image: "/src/assets/images/avatar-jacob-thompson.webp",
+    image: "/public/assets/images/avatar-jacob-thompson.webp",
     name: "Jacob Thompson",
     description: "has joined you group ",
     highlight: "Chess Club",
@@ -41,7 +41,7 @@ export const Data: DataProps[] = [
     id: 3,
   },
   {
-    image: "/src/assets/images/avatar-rizky-hasanuddin.webp",
+    image: "/public/assets/images/avatar-rizky-hasanuddin.webp",
     name: "Rizky Hasanuddin",
     description: "sent you a private message",
     date: "5day ago",
@@ -53,18 +53,18 @@ export const Data: DataProps[] = [
     id: 4,
   },
   {
-    image: "/src/assets/images/avatar-kimberly-smith.webp",
+    image: "/public/assets/images/avatar-kimberly-smith.webp",
     name: "Kimberly Smith",
     description: "commented on your picture",
     date: "1week ago",
     status: "completed",
     action: {
-      comment: "/src/assets/images/image-chess.webp",
+      comment: "/public/assets/images/image-chess.webp",
     },
     id: 5,
   },
   {
-    image: "/src/assets/images/avatar-nathan-peterson.webp",
+    image: "/public/assets/images/avatar-nathan-peterson.webp",
     name: "Nathan Peterson",
     description: "reacted to your recent pos ",
     highlight: "5 end-game strategies to increase your win rate",
@@ -73,7 +73,7 @@ export const Data: DataProps[] = [
     id: 6,
   },
   {
-    image: "/src/assets/images/avatar-anna-kim.webp",
+    image: "/public/assets/images/avatar-anna-kim.webp",
     name: "Anna Kim",
     description: "left the group ",
     highlight: "Chess Club",
